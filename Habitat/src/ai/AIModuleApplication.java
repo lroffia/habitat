150e015e234815128cdb2ac8f0922f152780d565
Marco Lippi <marcolippi83@gmail.com>
2016-07-27 09:03:01 +0200

    Demo with AI Module

---
+++ Habitat/src/ai/AIModuleApplication.java
@@ -0,0 +1,60 @@
+package it.unibo.disi.ai;
+
+import java.awt.EventQueue;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+public class AIModuleApplication {
+
+	private AIModule ai;
+	private JFrame frame;
+
+	public AIModuleApplication()
+	{
+		ai = new AIModule();
+		
+		if (ai.join() == false)
+		{
+			System.out.println("JOIN FAILED");
+			System.exit(0);
+		}
+		
+		if (ai.subscribe(null) == null)
+		{
+			System.out.println("SUBSCRIBE FAILED");
+			System.exit(0);
+		}
+		
+		frame = new JFrame();
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				if (ai != null) {
+					ai.unsubscribe();
+					ai.leave();
+					ai.terminate();
+				}
+			}
+		});
+		frame.setResizable(false);
+		frame.setTitle("HABITAT - AI Module");
+		frame.setBounds(300, 300, 200, 100);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AIModuleApplication aiModuleApplication = new AIModuleApplication();
+					aiModuleApplication.frame.setVisible(true);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
